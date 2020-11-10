876af428a8b4e8c8443c9eff74f86fdd2375b2ae
Anna <rosalenanna@gmail.com>
2020-11-10 17:13:51 +0100

    jsnack2 completato

---
+++ jsnack2/js/myscript.js
@@ -0,0 +1,37 @@
+// dato questo array di obj iniziale:
+// var arrayObj = [
+//   {name: 'Poppy', type: 'tshirt', color: 'red'},
+//   {name: 'Jumping', type: 'occhiali', color: 'blue'},
+//   {name: 'CrissCross', type: 'scarpe', color: 'black'},
+//   {name: 'Jenny', type: 'borsa', color: 'pink'},
+// ];
+// //Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array)
+// una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale
+
+var arrayObj = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
+
+var arrayCopia = arrayObj;
+
+
+for (var i = 0; i < arrayCopia.length; i++) {
+  var position = randomLetter(1);
+  arrayCopia[i].position = position;
+}
+
+console.log(arrayCopia);
+
+
+// funzione per  lettera random da inserire in position
+function randomLetter(num) {
+   var risultato = '';
+   var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   for ( var i = 0; i < num; i++ ) {
+      risultato = letters.charAt(Math.floor(Math.random() * letters.length));
+   }
+   return risultato;
+}
