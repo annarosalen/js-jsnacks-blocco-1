6bc2d0233f6323f292b10ec4f66995ae7e1e70a1
Anna <rosalenanna@gmail.com>
2020-11-10 16:10:12 +0100

    primo completato

---
+++ eserciziolive7/js/myscript.js
@@ -0,0 +1,36 @@
+// Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+
+var arraybici = [
+  {
+    'nome': "gialla",
+    'peso': 20
+  },
+  {
+    'nome': "verde",
+    'peso': 30
+  },
+  {
+    'nome': "rossa",
+    'peso': 15
+  },
+  {
+    'nome': "blue",
+    'peso': 11
+  },
+  {
+    'nome': "rosa",
+    'peso': 50
+  }
+];
+
+var piuLeggera = arraybici[0];
+
+for (var i = 0; i < arraybici.length; i++) {
+  if(piuLeggera.peso > arraybici[i].peso){
+    piuLeggera = arraybici[i];
+  }
+}
+
+console.log(piuLeggera);
+
+// oppure metti sort
